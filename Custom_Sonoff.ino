d04f7271bd982d117c79394d263ff85c9ee4310a
PRAKASH KUMAR <prakakum@adobe.com>
2021-01-27 13:09:07 +0530

    first commit

---
+++ Custom_Sonoff.ino
@@ -0,0 +1,127 @@
+#include <ESP8266WiFi.h>
+#include <Espalexa.h>
+
+
+// define the GPIO connected with Relays
+
+#define RelayPin1 13  //D1
+#define RelayPin2 12 //D2
+
+
+//direct callback functions
+void device1Changed(uint8_t brightness);
+void device2Changed(uint8_t brightness);
+
+
+// device names
+String Device_1_Name = "SmartSwitch";
+String Device_2_Name = "Green light";
+
+
+Espalexa espalexa;
+
+// prototypes
+boolean connectWifi();
+
+// WiFi Credentials
+const char* ssid = "MY_SSID";
+const char* password = "MY_SSID_Password";
+
+boolean wifiConnected = false;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+
+
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+
+  if (wifiConnected)
+  {
+    // Define your devices here.
+    espalexa.addDevice(Device_1_Name, device1Changed);
+
+
+    espalexa.begin();
+  }
+  else
+  {
+    while (1)
+    {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
+    }
+  }
+}
+
+void loop()
+{
+  espalexa.loop();
+  delay(1);
+}
+
+//our callback functions
+void device1Changed(uint8_t brightness){
+
+  //Control the device
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin1, HIGH);
+    digitalWrite(RelayPin2, HIGH);
+      Serial.println("Device1 ON");
+    }
+  else if (brightness == 0)
+  {
+    digitalWrite(RelayPin1, LOW);
+  digitalWrite(RelayPin2, LOW);
+    Serial.println("Device1 OFF");
+  }
+  else
+  {
+    int brigh_perc = (brightness/255.)*100;
+    analogWrite(RelayPin1, brightness);
+    Serial.print("Device1 Brightness: ");
+    Serial.print(brigh_perc);
+    Serial.println("%");
+    
+  }
+}
+
+
+// connect to wifi  returns true if successful or false if not
+boolean connectWifi()
+{
+  boolean state = true;
+  int i = 0;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20) {
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
+  if (state) {
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("Connection failed.");
+  }
+  return state;
+}
